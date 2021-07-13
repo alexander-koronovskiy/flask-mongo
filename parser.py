4188636974bbe57e625cb0c5efbdff842c311e1c
alexander k <alexander.koronovskiy@gmail.com>
2021-07-13 11:38:24 +0400

    base get json func test

--- parser.py
+++ parser.py
@@ -1,7 +1,20 @@
+import json
+
+import lxml
 import requests
 from bs4 import BeautifulSoup
 
 
-def parse_page(url: str):
+def parse_page(url: str) -> lxml:
     response = requests.get(url)
     return BeautifulSoup(response.text, "lxml")
+
+
+def get_exchange_rate(url: str) -> json:
+    return requests.get(url).json()
+
+
+# план работ
+# 1 поиск ресурса\ ресурсов для получения курса валют с помощью ф-ии get_exchange_rate
+# 2 запись в БД ВСЕХ найденных курсов
+# 3 пробная ф-ия для принта выбранного курса из БД
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,6 +1,11 @@
-from parser import parse_page
+from parser import get_exchange_rate, parse_page
 
 
 def test_parse_page():
     url = "https://markets.businessinsider.com/"
     assert parse_page(url)
+
+
+def test_get_exchange_rate():
+    url = f"https://randomuser.me/api/?results=5"
+    assert get_exchange_rate(url)
