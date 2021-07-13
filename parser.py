3653da35b686ed22a53c0f472e5d693f2a68f056
alexander k <alexander.koronovskiy@gmail.com>
2021-07-13 12:30:46 +0400

    before db work

--- parser.py
+++ parser.py
@@ -1,20 +1,15 @@
 import json
 
-import lxml
 import requests
-from bs4 import BeautifulSoup
 
 
-def parse_page(url: str) -> lxml:
-    response = requests.get(url)
-    return BeautifulSoup(response.text, "lxml")
-
-
-def get_exchange_rate(url: str) -> json:
-    return requests.get(url).json()
+def get_exchange_rate(url: str, keyword: str) -> json:
+    return requests.get(url).json()[keyword]
 
 
 # план работ
-# 1 поиск ресурса\ ресурсов для получения курса валют с помощью ф-ии get_exchange_rate
-# 2 запись в БД ВСЕХ найденных курсов
-# 3 пробная ф-ия для принта выбранного курса из БД
+
+# 0 другие ресурсы для получения курса валют ???
+
+# 1 работа с БД \ запись в БД ВСЕХ полученных курсов
+# 2 пробная ф-ия для принта выбранного курса из БД
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,11 +1,7 @@
-from parser import get_exchange_rate, parse_page
-
-
-def test_parse_page():
-    url = "https://markets.businessinsider.com/"
-    assert parse_page(url)
+from parser import get_exchange_rate
 
 
 def test_get_exchange_rate():
-    url = f"https://randomuser.me/api/?results=5"
-    assert get_exchange_rate(url)
+    rates = get_exchange_rate("https://www.cbr-xml-daily.ru/latest.js", "rates")
+    for key in rates:
+        assert key, rates[key]
