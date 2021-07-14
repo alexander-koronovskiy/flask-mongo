cbe9ee45e4a3aa9dd737a99e89437e01bcfdee0a
alexander k <alexander.koronovskiy@gmail.com>
2021-07-14 14:15:31 +0400

    code reformatting

--- handler.py
+++ handler.py
@@ -10,22 +10,22 @@ def get_outside_rates_old(url: str) -> json:
 
 # db connection
 def db_conn(col: str) -> database:
-    client = MongoClient("mongodb://localhost:27017/")
+    client = MongoClient('mongodb://localhost:27017/')
     return client[col]
 
 
 # rates recording from outside api to db
 def record_outside_rates(url: str, keyword: str):
-    col = db_conn("rates")["rates"]
+    col = db_conn('rates')['rates']
     rates = requests.get(url).json()[keyword]
     col.insert_one(rates)
     return rates
 
 
-# printing records from db
-rates = record_outside_rates("https://www.cbr-xml-daily.ru/latest.js", "rates")
-for row in db_conn("rates").rates.find():
-    print(row)
-
 # remove previous record
-print(help(db_conn("rates").rates))
+# print(db_conn("rates").rates.remove())
+
+# printing records from db
+rates = record_outside_rates('https://www.cbr-xml-daily.ru/latest.js', 'rates')
+for row in db_conn('rates').rates.find():
+    print(row)
--- tests/test_handler.py
+++ tests/test_handler.py
@@ -2,6 +2,6 @@ from handler import get_outside_rates_old
 
 
 def test_get_outside_rates_old():
-    rates = get_outside_rates_old("https://www.cbr-xml-daily.ru/latest.js")["rates"]
+    rates = get_outside_rates_old('https://www.cbr-xml-daily.ru/latest.js')['rates']
     for key in rates:
         assert key, rates[key]
