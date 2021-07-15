c3932e9c5cd2cc16e366850cce8d83ab46a3e3ef
alexander k <alexander.koronovskiy@gmail.com>
2021-07-15 17:45:35 +0400

    test index refractor
    add assertion for test_index method

--- tests/test_index.py
+++ tests/test_index.py
@@ -2,6 +2,6 @@ import requests
 
 
 def test_index():
-    requests.get('http://127.0.0.1:5000/').json()
+    assert requests.get('http://127.0.0.1:5000/').json()
 
 # add test of closed flask server case
