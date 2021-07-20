b8d487af2d2bf36489f1045b67cb1c4c5d738fdf
alexander k <alexander.koronovskiy@gmail.com>
2021-07-20 14:56:22 +0400

    client handler file reformat
    rformat methods by created wrap schema function in client handler file

--- client_handler.py
+++ client_handler.py
@@ -1,39 +1,32 @@
 import json
 from datetime import datetime
 
-# release schema, pop methods for id,
-# add test rub cases in test index, cl handler tests
+
+def wrap_schema(value: dict, id: str) -> dict:
+    return {'id': id,
+            'response': 200,
+            'timestamp': datetime.now().strftime('%d/%m/%Y %H:%M:%S'),
+            'rates': value,
+            }
+
 
 def index_wrapper(rates: json) -> json:
     """json response formation for index method"""
-    _id = str(rates['_id'])
-    del rates['_id']
-    return {'id': _id,
-            'response': 200,
-            'timestamp': datetime.now().strftime('%d/%m/%Y %H:%M:%S'),
-            'rates': rates,
-            }
+    _id = str(rates.pop('_id'))
+    return wrap_schema(rates, _id)
 
 
 def convert_all_wrapper(rates: json, to_rate_key: str) -> json:
     """json response formation for convert all method"""
-    _id = str(rates['_id'])
-    del rates['_id']
+    _id = str(rates.pop('_id'))
     currency = rates[to_rate_key]
-    return {'id': _id,
-            'response': 200,
-            'timestamp': datetime.now().strftime('%d/%m/%Y %H:%M:%S'),
-            'rates': {key: rates[key] / currency for key in rates.keys()}
-            }
+    value = {key: rates[key] / currency for key in rates.keys()}
+    return wrap_schema(value, _id)
 
 
 def convert_wrapper(rates: json, from_rate_key: str, to_rate_key: str) -> json:
     """json response formation for convert method"""
-    _id = str(rates['_id'])
-    del rates['_id']
-    return {'id': _id,
-            'response': 200,
-            'timestamp': datetime.now().strftime('%d/%m/%Y %H:%M:%S'),
-            'rates': {'from': from_rate_key, 'to': to_rate_key,
-                      'value': rates[to_rate_key] / rates[from_rate_key]}
-            }
+    _id = str(rates.pop('_id'))
+    value = {'from': from_rate_key, 'to': to_rate_key,
+             'value': rates[to_rate_key] / rates[from_rate_key]}
+    return wrap_schema(value, _id)
